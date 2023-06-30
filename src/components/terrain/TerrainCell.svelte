9d3cd9a32569879ebb72c43b3c03eb0dff34c05f
Ben Allfree <ben@benallfree.com>
2023-06-29 23:28:52 -0700

    asset editor on click

--- src/components/terrain/TerrainCell.svelte
+++ src/components/terrain/TerrainCell.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
   import type { TerrainCell } from '../../util/helpers'
+  import AssetEditor from '../AssetEditor/AssetEditor.svelte'
 
-  export let cell: TerrainCell | undefined
+  export let cell: TerrainCell
   const { x: r, y: c, height, type } = cell || {}
-  // console.log({ cell })
+
+  let modalOpen = false
+  const onClick = () => {
+    modalOpen = true
+  }
 </script>
 
-{#if cell}
-  <div class="cell" style={`background-color: ${cell.topographicalHeat()}`}>
-    <div>{r},{c}</div>
-    <div class={type}>{type}</div>
-    <div>{height}</div>
-  </div>
+{#if modalOpen}
+  <AssetEditor />
 {/if}
+<div class="cell" on:click={onClick}>
+  <div>{r},{c}</div>
+  <div class={type}>{type}</div>
+  <div>{height}</div>
+</div>
 
 <style lang="scss">
   .cell {
     width: 50px;
     height: 50px;
     border: 1px solid red;
-    display: inline-block;
+    float: left;
     font-size: 10px;
+    cursor: pointer;
     .R {
       width: 15px;
       height: 15px;
