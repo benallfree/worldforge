7c664acd53cf3709a4ef86e9624a18fcec8b61ad
Ben Allfree <ben@benallfree.com>
2023-06-29 23:28:33 -0700

    terrain refactor

--- src/components/terrain/TerrainMap.svelte
+++ src/components/terrain/TerrainMap.svelte
@@ -1,26 +1,20 @@
 <script lang="ts">
-  import { mkGridSize, xyToSlug } from '../../util/helpers'
-  import { ScreenNames, gameState } from '../../store'
+  import { gameState } from '../../store'
   import { TILE_SIZE } from '../../util/constants'
+  import { mkGridSize, xyToSlug } from '../../util/helpers'
   import { px } from '../../util/px'
   import { range } from '../../util/range'
   import ShareTool from './ShareTool.svelte'
   import TerrainCell from './TerrainCell.svelte'
 
-  const { navigate } = gameState
   export let size = mkGridSize($gameState.terrain.size())
 </script>
 
-<button on:click={() => navigate(ScreenNames.Editor)}>Edit Assets</button>
 <ShareTool />
 <div class="world" style={`width: ${px((TILE_SIZE + 2) * size)}`}>
-  {#each range(size) as r}
-    <div class="row">
-      {#each range(size) as c}
-        <TerrainCell cell={$gameState.terrain.get(xyToSlug(r, c))} />
-      {/each}
-    </div>
-  {/each}
+  {#each range(size) as r}{#each range(size) as c}<TerrainCell
+        cell={$gameState.terrain.get(xyToSlug(r, c))}
+      />{/each}{/each}
 </div>
 
 <style lang="scss">
