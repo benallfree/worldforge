bfb1e2c69530fe3a1125e414f8056b5a2770d4c5
Ben Allfree <ben@benallfree.com>
2023-07-04 02:53:01 -0700

    switch van back to minified mode

--- src/van/index.ts
+++ src/van/index.ts
@@ -1,4 +1,4 @@
-import van, { type ChildDom, type Props, type State } from './van-0.12.3.debug'
+import van, { type ChildDom, type Props, type State } from './van-0.12.3.min'
 
 export const { button, div, a, p, pre, br, input, img, sup, h1, textarea } = van.tags
 export const { state, bind } = van
