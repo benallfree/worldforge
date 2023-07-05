dd1fc6c3d74a65e656466e3053573cbe4b28df43
Ben Allfree <ben@benallfree.com>
2023-07-05 04:00:34 -0700

    App css refactor

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,13 +1,13 @@
 import buildInfo from '../../buildInfo.json'
 import { gameStore } from '../store/gameStore'
+import { CLEARFIX, mkClass } from '../util/mkClass'
 import { a, bind, div, h1 } from '../van'
-import classes from './App.module.scss'
 import { Game } from './Game'
 
 export const App = () => {
   const { loaded, modal } = gameStore
   return div(
-    { class: classes.App },
+    { ...mkClass(`App`, CLEARFIX) },
     bind(loaded, (loaded) => {
       if (loaded) {
         return Game()
@@ -15,7 +15,7 @@ export const App = () => {
       return h1(`WorldForge`)
     }),
     div(
-      { class: classes.footer },
+      { ...mkClass(`footer`) },
       a({ href: `https://github.com/benallfree/worldforge` }, `WorldForge`),
       ` build ${buildInfo.build}. Created with love under the MIT open source license. `
     ),
