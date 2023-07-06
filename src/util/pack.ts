d986200ac9be5839eeec4627127838e292a73850
Ben Allfree <ben@benallfree.com>
2023-07-06 11:41:11 -0700

    unpack fix

--- src/util/pack.ts
+++ src/util/pack.ts
@@ -5,7 +5,7 @@ export const pack = (payload: string) =>
   compress(payload).then((packed) => `${WF_PACK_HEADER}${packed}`)
 
 export const unpack = async (payload: string) => {
-  const [, blob] = payload.match(/WFDLC\*([a-zA-Z0-9%]+)/) || []
+  const [, blob] = payload.match(/WFDLC\*(.+)/) || []
   if (!blob) return
 
   return decompress(blob)
